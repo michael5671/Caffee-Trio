a6af6b90a4911f0aab5a05a16fa4eada5e3f81f9
ngntu10 <phamnguyentu04@gmail.com>
2025-05-27 23:09:24 +0700

    feat: update user DTO

--- src/main/java/com/ngntu10/dto/request/product/DeleteMultiProductDTO.java
+++ src/main/java/com/ngntu10/dto/request/product/DeleteMultiProductDTO.java
@@ -14,15 +14,5 @@ import io.swagger.v3.oas.annotations.media.Schema;
 @NoArgsConstructor
 @Schema(description = "DTO for deleting multiple products")
 public class DeleteMultiProductDTO {
-    @NotEmpty(message = "Product IDs cannot be empty")
-    @Schema(
-            name = "productIds",
-            description = "Danh sách ID sản phẩm",
-            type = "array",
-            requiredMode = Schema.RequiredMode.REQUIRED,
-            example = "[\"54505b04-6e56-4435-8870-eb968badf7ec\", " +
-                      "\"b79433f9-69fe-4289-825d-8865b693d13e\", " +
-                      "\"7e9aa8d1-3341-401b-a15a-3436dd163603\"]"
-    )
     private List<String> productIds;
 }
